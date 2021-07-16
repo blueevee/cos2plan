dc2f35e73e782d0953b94c70fc749dbe9600b165
Evelyn Souza <evelyn.ferreira@solutis.com.br>
2021-07-15 22:03:50 -0300

    feat: Get data of new cosplay page

--- src/pages/Main/Main.py
+++ src/pages/Main/Main.py
@@ -1,35 +1,31 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.boxlayout import MDBoxLayout, MDAdaptiveWidget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelThreeLine
 from kivy.uix.button import Button
 from kivy.core.window import Window
-from kivy.properties import StringProperty
-from kivymd.uix.card import MDCardSwipe
+from kivymd.uix.list import MDList, TwoLineListItem
+from kivy.uix.scrollview import ScrollView
 
 Window.size = (400,600)
 
-class SwipeToDeleteItem(MDCardSwipe):
-    text = StringProperty()
+class MyScreenManager(ScreenManager):
+    pass
 
-class MainScreen(MDBoxLayout):
+class MainScreen(Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
 
-    def build(self):
-        return self.screen
-
-    def on_swipe_complete(self, instance):
-        self.screen.ids.md_list.remove_widget(instance)
-
-    def on_start(self):
-        for i in range(20):
-            self.screen.ids.md_list.add_widget(
-                SwipeToDeleteItem(text=f"One-line item {i}")
-            )
-
+class NewCosplayScreen(Screen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+    
+    def show_data(self, name, origin, budget, url):
+        print([name, origin, budget, url])
 
 class Test(MDApp):
     def build(self):
-        return MainScreen()
+        return MyScreenManager()
 
 Test().run()
